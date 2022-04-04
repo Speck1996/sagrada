f92ae8a744af3e8d23434080f28e71b2a4afa19e
Speck1996 <37180716+Speck1996@users.noreply.github.com>
2022-04-04 14:28:23 +0200

    Delete launch_registry - windows (speck).sh

--- launch_registry - windows (speck).sh
+++
@@ -1,6 +0,0 @@
-cd C:/Users/kepec/Documents/GitHub/sagrada/ing-sw-2018-Pecchia-Peretti-Tibaldi/ing-sw-2018-Pecchia-Peretti-Tibaldi/target/classes/
-start rmiregistry\
-     -J-Djava.rmi.server.logCalls=true \
-     -J-Djava.rmi.server.useCodebaseOnly=false \
-     -J-Djava.rmi.server.codebase=file:///C:/Users/kepec/Documents/GitHub/sagrada/ing-sw-2018-Pecchia-Peretti-Tibaldi/ing-sw-2018-Pecchia-Peretti-Tibaldi/target/classes/
-
